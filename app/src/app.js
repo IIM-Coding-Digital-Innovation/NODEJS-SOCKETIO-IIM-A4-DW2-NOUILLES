1795a2a597bca2d36eca3981aff2d406e9e57e56
Julie Souchet <jsouchet@easyvista.com>
2022-04-26 15:02:10 +0200

    style(eslint): add eslint and prettier

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = process.env.SERVER_PORT
+const app = express();
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req, res) => {
-    res.send('Goodnight World!')
-})
+  res.send('Goodnight World!');
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on http://127.0.0.1:${port}`)
-})
+  console.log(`Server listening on http://127.0.0.1:${port}`);
+});
