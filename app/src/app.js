b06ceaabcc6b972a2a565adaf4c6b5d427f8dde8
Naoufel <naoufel.maazouzi@live.fr>
2022-04-26 15:00:15 +0200

    add connexion to postgre databse

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { sequelizeInstance } = require('./utils/database');
 
 const app = express()
 const port = process.env.SERVER_PORT
@@ -7,6 +8,16 @@ app.get('/', (req, res) => {
     res.send('Goodnight World!')
 })
 
-app.listen(port, () => {
-    console.log(`Server listening on http://127.0.0.1:${port}`)
-})
+const startServer = async () => {
+    try {
+        await sequelizeInstance.authenticate();
+        console.log('Connection with database has been established successfully.');
+        app.listen(port, () => {
+            console.log(`Server listening on http://127.0.0.1:${port}`);
+        });
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+startServer();
