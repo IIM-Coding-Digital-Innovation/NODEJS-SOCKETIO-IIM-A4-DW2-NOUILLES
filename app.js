3a3ec615cf5651df984b0153239527666c4a5cf9
Denporty <victor.cabot@edu.devinci.fr>
2022-04-27 14:36:00 +0200

    Try to fix

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 //const { sequelizeInstance } = require('./utils/database');
 const path = require("path");
 const userAPI = require('./src/api/user/routes')
-const sequelize = require('./models/index')
+const db = require('./models/index')
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+} else if ( process.env.NODE_ENV !== 'develop') {
+  require('dotenv').config()
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/src/index.html'))
@@ -19,7 +25,7 @@ userAPI.createRoutes(app);
 
 async function main() {
   try {
-    await sequelize.authenticate();
+    await db.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
--- models/index.js
+++ models/index.js
@@ -5,16 +5,22 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(dirname + '/../config/config.json')[env];
+const config = require(dirname + '/config/config.json')[env];
 const db = {};
 
-const {Post, Author} = require('../../models');
+const {Post, Author} = require('/models');
 
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.env) {
+    sequelize = new Sequelize(process.env[config.env], config);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            dialectOptions: {
+            ssl: { require: true, rejectUnauthorized: false }
+        }
+    });
 }
 
 fs
