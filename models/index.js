3d74f4193134722010a636ea05856139fb93c388
Denporty <victor.cabot@edu.devinci.fr>
2022-04-27 14:45:56 +0200

    Fix

--- models/index.js
+++ models/index.js
@@ -2,21 +2,17 @@
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require('/config/config.json')[env];
+//const config = require('/config/config.json')[env];
 
 const {Post, Author} = require('/models');
 
 let sequelize;
-if (process.env.NODE_ENV !== 'develop') {
-    sequelize = new Sequelize(process.env[config.env], config);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-            host: process.env.DB_HOST,
-            dialect: 'postgres',
-            dialectOptions: {
-            ssl: { require: true, rejectUnauthorized: false }
-        }
-    });
-}
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false }
+    }
+});
 
 module.exports = sequelize;
