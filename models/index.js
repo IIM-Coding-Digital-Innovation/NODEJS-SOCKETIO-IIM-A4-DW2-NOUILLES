0ebb972e930d6baf7fb97f72422214a76c84a41b
Denporty <victor.cabot@edu.devinci.fr>
2022-04-27 14:39:57 +0200

    Try to fix

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(dirname + '/config/config.json')[env];
 const db = {};
@@ -23,23 +20,4 @@ if (config.env) {
     });
 }
 
-fs
-    .readdirSync(dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        const model = require(path.join(dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
-
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = sequelize;
