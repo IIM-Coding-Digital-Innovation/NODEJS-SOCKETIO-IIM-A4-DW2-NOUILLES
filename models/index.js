e552a492ccccc5809368ff6a72373cb9e9bee9eb
Denporty <victor.cabot@edu.devinci.fr>
2022-04-27 15:22:28 +0200

    Fix

--- models/index.js
+++ models/index.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(filename);
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
@@ -16,12 +16,12 @@ if (config.use_env_variable) {
 }
 
 fs
-    .readdirSync(dirname)
+    .readdirSync(__dirname)
     .filter(file => {
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
-        const model = require(path.join(dirname, file))(sequelize, Sequelize.DataTypes);
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
 
