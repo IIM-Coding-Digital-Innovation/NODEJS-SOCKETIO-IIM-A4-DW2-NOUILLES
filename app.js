d653d0d80f42e64809d74822cd3b5596297921e0
Pierre Secher <pierresecher@sweetpunk.com>
2022-04-28 09:21:28 +0200

    init socket front + back

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const express = require('express');
+var http = require('http');
 //const { sequelizeInstance } = require('./utils/database');
 const path = require("path");
-const userAPI = require('./src/api/user/routes')
-const { sequelize } = require('./models/index')
-const bp = require('body-parser')
-
+const userAPI = require('./src/api/user/routes');
+const sequelize = require('./models/index');
+const bodyParser = require('body-parser');
 const app = express();
+const server = new http.Server(app);
+const io = require('socket.io')(server);
+
 const port = process.env.SERVER_PORT;
 
-//Formatted body 
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/index.html'))
-})
+  res.sendFile(path.join(__dirname, '/src/index.html'));
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Example app listening on port 3000`)
-})
+const messages = [];
+io.on('connection', (socket) => {
+  console.log(`Connecté au client ${socket.id}`);
+  socket.emit('messages', messages);
+  socket.on('message', (message) => {
+    if (messages.length > 10) {
+      messages.shift();
+    }
+    messages.push(message);
+    io.emit('messages', messages);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Votre app est disponible sur localhost:3000 !');
+});
 
 userAPI.createRoutes(app);
 
